ce6c17a8dd2a5632f543f7dcd957ca00e1f86ce7
duarte cota <duartenunocota@gmail.com>
2021-01-20 10:18:01 -0100

    update

--- Operações/src/Model/Opera.java
+++ Operações/src/Model/Opera.java
@@ -1,15 +1,18 @@
 package Model;
 
 public class Opera {
-
+	
+	//atributos
 	private double op1;
 	private double op2;
-
-	public Opera(int op1, int op2) {
+	
+	//construtor
+	public Opera(double op1, double op2) {
 		this.op1 = op1;
 		this.op2 = op2;
 	}
 
+	//getters e setters
 	public double getOp1() {
 		return op1;
 	}
@@ -25,7 +28,8 @@ public class Opera {
 	public void setOp2(int op2) {
 		this.op2 = op2;
 	}
-
+	
+	//métodos para manipular os atributos
 	public double somar() {
 		return this.op1 + this.op2;
 	}
--- Operações/src/application/Controller.java
+++ Operações/src/application/Controller.java
@@ -1,7 +1,9 @@
 package application;
 
+import Model.Opera;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
@@ -29,31 +31,142 @@ public class Controller {
     @FXML
     private RadioButton div;
     
-
+    private ToggleGroup tg;
+    
     @FXML
     void dividir(ActionEvent event) {
-    	
+    	try {
+    		if(this.op1.getText()=="") {
+    			this.op1.setText("0");
+    		}
+    		if(this.op2.getText()=="") {
+    			this.op2.setText("0");
+    		}
+    		double op1 = Double.parseDouble(this.op1.getText());
+        	double op2 = Double.parseDouble(this.op2.getText());
+        	Opera opera = new Opera(op1,op2);
+        	this.res.setText(String.valueOf(opera.dividir()));
+    	} catch (NumberFormatException e) {
+    		Alert alerta = new Alert(Alert.AlertType.ERROR);
+			alerta.setHeaderText(null);
+			alerta.setTitle("Ocorreu um erro!");
+			alerta.setContentText("Formato incorreto...");
+			alerta.showAndWait();
+			reset();
+    	}
     }
 
     @FXML
     void multiplicar(ActionEvent event) {
-    	
+    	try {
+    		if(this.op1.getText()=="") {
+    			this.op1.setText("0");
+    		}
+    		if(this.op2.getText()=="") {
+    			this.op2.setText("0");
+    		}
+    		double op1 = Double.parseDouble(this.op1.getText());
+        	double op2 = Double.parseDouble(this.op2.getText());
+        	Opera opera = new Opera(op1,op2);
+        	this.res.setText(String.valueOf(opera.multiplicar()));
+    	} catch (NumberFormatException e) {
+    		Alert alerta = new Alert(Alert.AlertType.ERROR);
+			alerta.setHeaderText(null);
+			alerta.setTitle("Ocorreu um erro!");
+			alerta.setContentText("Formato incorreto...");
+			alerta.showAndWait();
+			reset();
+    	}
     }
 
     @FXML
     void somar(ActionEvent event) {
+    	try {
+    		if(this.op1.getText()=="") {
+    			this.op1.setText("0");
+    		}
+    		if(this.op2.getText()=="") {
+    			this.op2.setText("0");
+    		}
+    		double op1 = Double.parseDouble(this.op1.getText());
+        	double op2 = Double.parseDouble(this.op2.getText());
+        	Opera opera = new Opera(op1,op2);
+        	this.res.setText(String.valueOf(opera.somar()));
+    	} catch (NumberFormatException e) {
+    		Alert alerta = new Alert(Alert.AlertType.ERROR);
+			alerta.setHeaderText(null);
+			alerta.setTitle("Ocorreu um erro!");
+			alerta.setContentText("Formato incorreto...");
+			alerta.showAndWait();
+			reset();
+    	}
     		
     }
 
     @FXML
     void subtrair(ActionEvent event) {
-    
+    	try {
+    		if(this.op1.getText()=="") {
+    			this.op1.setText("0");
+    		}
+    		if(this.op2.getText()=="") {
+    			this.op2.setText("0");
+    		}
+    		double op1 = Double.parseDouble(this.op1.getText());
+        	double op2 = Double.parseDouble(this.op2.getText());
+        	Opera opera = new Opera(op1,op2);
+        	this.res.setText(String.valueOf(opera.subtrair()));
+    	} catch (NumberFormatException e) {
+    		Alert alerta = new Alert(Alert.AlertType.ERROR);
+			alerta.setHeaderText(null);
+			alerta.setTitle("Ocorreu um erro!");
+			alerta.setContentText("Formato incorreto...");
+			alerta.showAndWait();
+			reset();
+    	}
     }
     
-    private ToggleGroup tg;
+    @FXML
+    void limpar(ActionEvent event) {
+    	this.op1.setText("");
+    	this.op2.setText("");
+    	this.res.setText("");
+    	if(this.soma.isSelected()) {
+    		this.soma.setSelected(false);
+    	}
+    	else if(this.sub.isSelected()) {
+    		this.sub.setSelected(false);
+    	}
+    	else if(this.div.isSelected()) {
+    		this.div.setSelected(false);
+    	}
+    	else if(this.mult.isSelected()) {
+    		this.mult.setSelected(false);
+    	}
+    }
+    
+    public void reset() {
+    	this.op1.setText("");
+    	this.op2.setText("");
+    	this.res.setText("");
+    	if(this.soma.isSelected()) {
+    		this.soma.setSelected(false);
+    	}
+    	else if(this.sub.isSelected()) {
+    		this.sub.setSelected(false);
+    	}
+    	else if(this.div.isSelected()) {
+    		this.div.setSelected(false);
+    	}
+    	else if(this.mult.isSelected()) {
+    		this.mult.setSelected(false);
+    	}
+	}
     
     public void initialize() {
+    	//instanciar um togglegroup
     	this.tg = new ToggleGroup();
+    	
     	this.soma.setToggleGroup(tg);
     	this.sub.setToggleGroup(tg);
     	this.mult.setToggleGroup(tg);
