acf3cbdc07c4e3625c925de59d9dea21601f400b
dcota <duartenunocota@gmail.com>
2021-01-28 13:31:23 -0100

    update aula 27jan2021

--- Tabelas2Views/src/controllers/Controller1.java
+++ Tabelas2Views/src/controllers/Controller1.java
@@ -1,4 +1,30 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller1 {
+
+    @FXML
+    private Button btnView2;
+
+    @FXML
+    void mudaView2(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/view2.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
+    }
+
 }
